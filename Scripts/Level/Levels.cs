957ab09c78b33916369e9e3309c3dd6ea212e1fe
Hiren Sameja <hirensameja007@gmail.com>
2023-09-16 14:26:07 +0530

    [ADD] Test Script

--- Scripts/Level/Levels.cs
+++ Scripts/Level/Levels.cs
@@ -3,13 +3,13 @@ using Godot.Collections;
 
 public partial class Levels : Node
 {
-    [Export()] private Array<LevelData> _levels = new Array<LevelData>();
+	[Export()] private Array<LevelData> _levels = new Array<LevelData>();
 
-    public LevelData GetLevelData(int index)
-    {
-        if (_levels.Count < index)
-            return new LevelData();
-        
-        return _levels[index];
-    }
+	public LevelData GetLevelData(int index)
+	{
+		if (_levels.Count < index)
+			return new LevelData();
+		
+		return _levels[index];
+	}
 }
---
+++ Scripts/Level/TempS.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public partial class TempS : Node
+{
+	[Export()] public int _winningIndex;
+}
