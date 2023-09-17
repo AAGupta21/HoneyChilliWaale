54045cb94a54111b0fb03a6a4fd8d1f919284f5c
AakashGupta-Ptw <aakashg219605@gmail.com>
2023-09-17 06:40:07 +0530

    Add popups for other button,  add button to quit app

--- Scripts/Manager.cs
+++ Scripts/Manager.cs
@@ -3,9 +3,19 @@ using Godot;
 public partial class Manager : Node
 {
     [Export()] private Button _playButton;
+    
+    [Export()] private Sprite2D _popupSprite;
+    [Export()] private Sprite2D _howToPlaySprite;
+    [Export()] private Sprite2D _creditsSprite;
+    [Export()] private Sprite2D _exitApplicationSprite;
+
     [Export()] private Button _howToPlayButton;
     [Export()] private Button _creditsButton;
-    [Export()] private Button _exitButton;
+    [Export()] private Button _exitApplicationPopupButton;
+    [Export()] private Button _agreeApplicationQuitButton;
+    [Export()] private Button _disagreeApplicationQuitButton;
+    [Export()] private Button _closePopupButton;
+
 
     [Export()] private Node2D _mainMenu;
     [Export()] private string _levelGeneratorPath;
@@ -15,9 +25,13 @@ public partial class Manager : Node
     {
         base._Ready();
         _playButton.ButtonUp += PlayButtonOnButtonUp;
-        _howToPlayButton.ButtonUp += PlayButtonOnButtonUp;
-        _creditsButton.ButtonUp += PlayButtonOnButtonUp;
-        _exitButton.ButtonUp += PlayButtonOnButtonUp;
+        _howToPlayButton.ButtonUp += HowToPlayButton;
+        _creditsButton.ButtonUp += CreditViewButton;
+        _exitApplicationPopupButton.ButtonUp += ExitApplicationPopupButton;
+        _closePopupButton.ButtonUp += ClosePopupButton;
+        _agreeApplicationQuitButton.ButtonUp += ApplicationQuitYesButton;
+        _disagreeApplicationQuitButton.ButtonUp += ApplicationQuitNoButton;
+
         _mainMenu.Visible = true;
     }
 
@@ -33,19 +47,61 @@ public partial class Manager : Node
     private void HowToPlayButton()
     {
         GD.Print("How TO PLay Tab");
+        
+        PopupScreenVisiblity((true));
+        _howToPlaySprite.Visible = true; 
+        
     }
     
     private void CreditViewButton()
     {
         GD.Print("Credit View Tab");
-        
+        PopupScreenVisiblity((true));
+        _creditsSprite.Visible = true; 
     }
     
-    private void ExitApplicationButton()
+    private void ExitApplicationPopupButton()
     {
+        GD.Print("Exit application tab");
+        PopupScreenVisiblity((true));
+        _exitApplicationSprite.Visible = true;
+
+    }
+
+    private void PopupScreenVisiblity(bool state)
+    {
+        _mainMenu.Visible = !state;
+        _popupSprite.Visible = state;
+    }
+
+    private void ClosePopupButton()
+    {
+        _howToPlaySprite.Visible = false; 
+        _creditsSprite.Visible = false; 
+        _exitApplicationSprite.Visible = false;
+        PopupScreenVisiblity((false));
         GD.Print("Exit application tab");
 
     }
+    
+    private void ApplicationQuitYesButton()
+    {
+        GD.Print("Exit application tab");
+        GetTree().Quit();
+    }
+    
+    private void ApplicationQuitNoButton()
+    {
+        _howToPlaySprite.Visible = false; 
+        _creditsSprite.Visible = false; 
+        _exitApplicationSprite.Visible = false;
+        PopupScreenVisiblity((false));
+
+        GD.Print("Exit application tab");
+
+    }
+    
+    
     
     
 }
